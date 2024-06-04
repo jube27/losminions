abdc0b00769f061dcae52dc44d3d431a8fde3c1f
jube27 <jubecuore1@gmail.com>
2024-06-04 11:26:06 -0500

    ejercicios

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -2,139 +2,67 @@
 //alumna: Julia Benavides , ejercicios js
 
 /*---------Ejercicio 1 ----------*/ 
-
 /*  años bisiestos
 1980, 1984, 1988, 1992, 1996, 2000, 2004, 2008, 2012, 2016, 
 2020 ,2028, 2032, 2036, 2040, 2044, 2048, 2052, 
 2056, 2060, 2064, 2068, 2072, 2076, 2080, 2084, 2088,
  2092 y 2096  */
 
-//.......declaracion de Funciones
-
-function bisiesto(ano) {
-    if ((+ano < 1900)||(+ano >2100)) return false;
-    return (((+ano % 4 == 0) && (+ano % 100 != 0)) || (+ano % 400 == 0))
-}
-
-
-
-
-//.....declaracion de Variables
-
+function bisiesto() {
     let anio1 = document.getElementById('anio'); 
-    const btncomprobar = document.getElementById("btncomprobar")
-    let container =document.getElementById("container");
-    let h2=document.querySelector("h2");
-    let p1 = document.createElement("p1");
-
-    btncomprobar.addEventListener("click", function (event) {
-        event.preventDefault()
-        p1.innerText = " ";
-        if(anio1.value)  {
-            if (bisiesto(anio1.value)){
-                p1.innerText =anio1.value + " es un año bisiesto";
-                container.insertBefore(p1, h2);
-            }else{
-                p1.innerText =anio1.value + " No es un año bisiesto ";
-                container.insertBefore(p1,h2);
-            }
-        }else {
-            p1.innerText ="ingrese un año ";
-        }    
-    })
-
+    if (((+anio1.value % 4 == 0) && (+anio1.value % 100 != 0)) || (+anio1.value % 400 == 0)) {   
+        document.getElementById("container1").innerText = `${anio1.value} es un año bisiesto `;
+    }
+    else{
+        document.getElementById("container1").innerText = `${anio1.value} no es un año bisiesto `;
+    }
+}
 
 
 /*  --------------- Ejercicio 2 -------------*/ 
+function calcularOperacion() {
 
-const x = document.getElementById('num1'); 
-const y = document.getElementById('num2'); 
-const operacion = document.getElementById('selecoper'); 
-const btnoperacion = document.getElementById("btnoperacion")
-let container2 =document.getElementById("container2");
-let p2 = document.createElement("p1");
-let resultadoCalculadora = 0;
-console.log(operacion.value);
+    const x = parseFloat(document.getElementById('numero1').value); 
+    const y = parseFloat(document.getElementById('numero2').value); 
+    const operacion = document.getElementById("selecoper").value; 
+    let resultadoCalculadora=0;
 
-btnoperacion.addEventListener("click", function (event) {
-        event.preventDefault()
-        p2.innerText = " ";
-        if(operacion.value === "+"){
-            p2.innerText = +x.value + +y.value;
-           container2.setAttribute("value", p2.innerText);
-            //container2.insertBefore(p2,document.querySelector("h2"));
-            //console.log("resultado suma: ", p2.innerText)
-        }else {
-            console.error("-1")
-        }
-    })
-
-
-
-/*
-console.log("ejercicio 2 ")
-const operacion="%"; // *
-const x=20;
-const y=4;
-
-let resultadoCalculadora = 0
-
-if(operacion === "+"){
-    resultadoCalculadora = +x + +y;
-    console.log("resultado suma: ", resultadoCalculadora)
-}else if (operacion === "-"){
-    resultadoCalculadora = +x - +y;
-    console.log("resultado resta: ", resultadoCalculadora)
-}else if (operacion === "*"){
-    resultadoCalculadora = +x * +y;
-    console.log("resultado multiplicacion: ", resultadoCalculadora)
-}else if (operacion === "/"){
-    if(+y !== 0){
-        resultadoCalculadora = +x / +y;
-        console.log("resultado division: ", resultadoCalculadora)
-    }else {
-        console.warn("el divisor debe ser distinto de cero")
+    if(operacion === "+"){
+        resultadoCalculadora = x + y;
+        
+    }else if (operacion === "-"){
+        resultadoCalculadora = x - y;
+    }else if (operacion === "*"){
+        resultadoCalculadora = x * y;
+    }else if (operacion === "/"){
+        if( y !== 0)
+            resultadoCalculadora = x / y;
+        else
+            resultadoCalculadora = "el divisor debe ser distinto de cero";
+    }else if (operacion === "^"){
+        resultadoCalculadora = Math.pow(x,y);
+    }else if (operacion === "%"){
+        resultadoCalculadora = x % y;
     }
-}else if (operacion === "^"){
-    resultadoCalculadora = Math.pow(+x, +y);
-    console.log("resultado exponenciacion: ", resultadoCalculadora)
-}else if (operacion === "%"){
-    resultadoCalculadora = +x % +y;
-    console.log("resultado residuo: ", resultadoCalculadora)
-}else {
-    console.error("-1")
+
+    document.getElementById("container2").innerText = `El resultado de la operacion es: ${resultadoCalculadora}`;
+ 
 }
-*/
+
 
 /*  --------------------- Ejercicio 3 -------------------*/ 
-
-console.log(".....ejercicio 3 .......")
-
-let nombre1="cerveza 2.50 soles";
-let nombre2="papitas 5.00 soles";
-
-console.log("cerveza 2.50 soles");
-console.log("papitas 5.00 soles");
-
-function calcularPrecio (nombre, cantidad){
-    var subcadena;
-    var indice1, indice2; 
-    indice1 = nombre.indexOf(" ")+1;
-    console.log(indice1);
-    subcadena1= nombre.substring(indice1, nombre.length);
-    console.log(subcadena1);
-    indice2 = subcadena1.indexOf(" ")+1;
-    subcadena2= subcadena1.substring( 0, indice2);
-    
-    return (+subcadena2)*cantidad;
+function calcularPrecioTotal(){
+    var precio1=0, precio2=0, precio3=0;
+    let sumaTotal;
+    if(document.querySelector('input[name="checkbox1"]:checked')){
+        precio1 = parseFloat(document.getElementById("checkbox1").value) * parseFloat(document.getElementById("cantidad1").value);   
+    };
+    if(document.querySelector('input[name="checkbox2"]:checked')){
+        precio2 = parseFloat(document.getElementById("checkbox2").value) * parseFloat(document.getElementById("cantidad2").value);
+    };
+    if(document.querySelector('input[name="checkbox3"]:checked')){
+        precio3 = parseFloat(document.getElementById("checkbox3").value) * parseFloat(document.getElementById("cantidad3").value);
+    };
+    sumaTotal = precio1 + precio2 + precio3;     
+    document.getElementById("container3").innerText = `El precio total es: ${sumaTotal}`;
 }
-let cantidad1=2;
-let cantidad2=3;
-console.log("cantidades respectivamente ",cantidad1 ,"  ",cantidad2);
-let suma = calcularPrecio (nombre1, cantidad1) + calcularPrecio (nombre2, cantidad2)
-console.log("precio total "+ suma);
-
-
-
-
-
